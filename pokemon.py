d0b54ecd32537b6900c172dc4173fc510b60e67d
Minu Choi <cmw@Minus-MacBook-Air.local>
2024-09-21 15:56:29 -0400

    Train/Level Up Functions added

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,4 @@
+import random
 from typing import List, Tuple
 
 class Pokemon:
@@ -19,12 +20,27 @@ class Pokemon:
         self.fight_status: bool = False
         self.alive: bool = True
 
+    def train(self):
+        experience_gain = random.randint(5, 13)
+        self.experience_points += experience_gain
+        print(f"{self.name} gained {experience_gain} experience points!")
+        
+        if self.experience_points >= 100:
+            self.level_up()
+    
+    def level_up(self):
+        self.level += 1
+        self.experience_points = 0
+        print(f"{self.name}'s body seems to be growing!")
+        print(f"{self.name} has leveled up to level {self.level}!")
+        
+
 class FightingPokemon(Pokemon):
     def __init__(self, name: str, pokedexid: str, level: int, hp: int, attack_points: int, defense_points: int, 
                  special_attack_points: int, special_defense_points: int, speed: int, experience_points: int,
                  skill: List[Tuple[str, int]]) -> None:
         
-        #Fighting-type Pokemons receive a 20% increase in attack points
+        # Fighting-type Pokemons receive a 20% increase in attack points
         attack_points = int(attack_points * 1.2)
         
         super().__init__(name, pokedexid, level, hp, attack_points, defense_points, 
@@ -46,7 +62,7 @@ class AquaPokemon(Pokemon):
                          special_attack_points, special_defense_points, speed, experience_points, skill)
         self.type = "Aqua"
     
-    def display_tyoe(self):
+    def display_type(self):
         print(f"{self.name} is a {self.type}-type Pokémon.")
 
 class ElectricPokemon(Pokemon):
@@ -59,14 +75,26 @@ class ElectricPokemon(Pokemon):
         super().__init__(name, pokedexid, level, hp, attack_points, defense_points, 
                          special_attack_points, special_defense_points, speed, experience_points, skill)
         self.type = "Electric"
+
+    def display_type(self):
+        print(f"{self.name} is a {self.type}-type Pokémon.")
+
+class GrassPokemon(Pokemon):
+    def __init__(self, name: str, pokedexid: str, level: int, hp: int, attack_points: int, defense_points: int, 
+                 special_attack_points: int, special_defense_points: int, speed: int, experience_points: int,
+                 skill: List[Tuple[str, int]]) -> None:
         
+        special_defense_points = int(special_defense_points * 1.3)
         
+        super().__init__(name, pokedexid, level, hp, attack_points, defense_points, 
+                         special_attack_points, special_defense_points, speed, experience_points, skill)
+        self.type = "Grass" 
+    
+    def display_type(self):
+        print(f"{self.name} is a {self.type}-type Pokémon.")
+
 
 machop = FightingPokemon('Machop', '040', 1, 70, 80, 50, 35, 35, 35, 0, [('Revenge', 60), ('Low Sweep', 60)])
 piplup = AquaPokemon('piplup', '007', 1, 53, 51, 53, 61, 56, 40, 0, [('water_gun', 40), ('peck', 35)])
 shinx = ElectricPokemon('shinx', '017', 1, 45, 65, 34, 40, 34, 45, 0, [('thunder_shock', 40), ('bite', 60)])
-
-
-
-
-#print(f"Pokemon Name: {machop.name}, Type: {machop.type}, HP: {machop.hp}, Attack Points: {machop.attack_points}, Skills: {machop.skill}")
+turtwig = GrassPokemon('Turtwig', '001', 1, 55, 68, 64, 45, 55, 31, 0, [('absorb', 20), ('razor_leaf', 55)])
